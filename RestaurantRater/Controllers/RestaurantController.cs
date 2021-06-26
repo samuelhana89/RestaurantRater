04dc8f5e3e42dd6da79b34425aec4e6d8d95571b
samuelhana89 <78116893+samuelhana89@users.noreply.github.com>
2021-06-26 16:32:32 -0400

    Create

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -11,10 +11,33 @@ namespace RestaurantRater.Controllers
     {
         private RestaurantDbContext _db = new RestaurantDbContext();
 
-        // GET: Restaurant
+        // GET: Restaurant/Index
         public ActionResult Index()
         {
             return View(_db.Restaurants.ToList());
         }
+
+        //Get : REstaurant /Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Post : REstaurant /Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
     }
+
 }
